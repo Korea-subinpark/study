1f9a917666d71a85ac6e97e6931fe9d85ba62850
Korea-subinpark <45931854+Korea-subinpark@users.noreply.github.com>
2019-01-15 23:50:39 +0900

    Update Solution_3307_최장증가부분수열_박수빈.java

--- algo/SWEA/D3/Solution_3307_최장증가부분수열_박수빈.java
+++ algo/SWEA/D3/Solution_3307_최장증가부분수열_박수빈.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
+ /*
+ 현재 원소보다 작은 이전 원소들 중에서 가장 큰 dp값을 구하여 1을 더한다
+ */
 public class Solution {
  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         
-        int tc = sc.nextInt();
-        for(int t=1; t<=tc; t++) {
+        int T = sc.nextInt();
+        for(int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt();
             int[] arr = new int[N];
             int[] dp = new int[N];
-            for(int i=0; i<N; i++)
+            for(int i = 0; i < N; i++)
                 arr[i] = sc.nextInt();
-            int maxCnt = 0, answer = 0;
-            for(int i=0; i<N; i++) {
-                for(int j=i-1; j>=0; j--) {
+            dp[0] = 1;
+            int max = 0;//현재 원소보다 작은 수 중 dp값의 최대값
+            int ans = 0;//dp 배열의 최대값
+            for(int i = 1; i < N; i++) {
+                for(int j = i - 1; j >= 0; j--) {
                     if(arr[i] > arr[j]) {
-                        if(dp[j] > maxCnt)
-                            maxCnt = dp[j];
+                        if(dp[j] > max)
+                            max = dp[j];
                     }
                 }
-                if(maxCnt == 0)
-                    dp[i] = 1;
-                else
-                    dp[i] = maxCnt + 1;
-                maxCnt = 0;
-                if(dp[i] > answer)
-                    answer = dp[i];
+                dp[i] = max + 1;
+                max = 0;
+                if(dp[i] > ans)
+                    ans = dp[i];
             }
-             
-            System.out.println("#" + t + " " + answer);
-             
+            System.out.println("#" + tc + " " + ans);
         }
+         
         sc.close();
     }
  
