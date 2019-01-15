c410f359b62f7b32d608728cdc707b740a6e6ad3
Korea-subinpark <45931854+Korea-subinpark@users.noreply.github.com>
2019-01-15 23:14:30 +0900

    Update Solution_5215_햄버거다이어트_박수빈.java

--- algo/SWEA/D3/Solution_5215_햄버거다이어트_박수빈.java
+++ algo/SWEA/D3/Solution_5215_햄버거다이어트_박수빈.java
@@ -1,39 +1,43 @@
+/*
+재료에 대한 점수와 칼로리를 배열에 저장하고
+현재 재료를 포함하는 dfs와 포함하지 않는 dfs를 모두 호출하여 최대 점수를 계산한다
+*/
 import java.util.Scanner;
- 
-public class Solution {
-    static int N, maxKal, maxScore;
-    static int[] score;
-    static int[] kal;
-     
-    static void dfs(int idx, int scoreVal, int kalVal) {
-        if(kalVal > maxKal)
-            return;
-        if(idx == N) {
-            if(scoreVal > maxScore)
-                maxScore = scoreVal;
-            return;
-        }
-        dfs(idx+1, scoreVal+score[idx], kalVal+kal[idx]);
-        dfs(idx+1, scoreVal, kalVal);
-    }
- 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        for(int t=1; t<=tc; t++) {
-            N = sc.nextInt();
-            score = new int[N];
-            kal = new int[N];
-            maxKal = sc.nextInt();
-            maxScore = 0;
-            for(int i=0; i<N; i++) {
-                score[i] = sc.nextInt();
-                kal[i] = sc.nextInt();
-            }
-            dfs(0,0,0);
-            System.out.println("#" + t + " " + maxScore);
-        }
-        sc.close();
-    }
- 
+
+public class Solution_5215_햄버거다이어트_박수빈 {
+	static int lim, maxScore;//칼로리 제한, 최대점수
+	static int[][] food;//재료의 점수와 칼로리
+	static int N;//재료 수
+	
+	public static void dfs(int i, int totalKal, int totalScore) {
+		if(totalKal <= lim && maxScore < totalScore)
+			maxScore = totalScore;
+		if(i + 1 >= N)
+			return;
+		dfs(i + 1, totalKal, totalScore);
+		dfs(i + 1, totalKal + food[i + 1][1], totalScore + food[i + 1][0]);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			N = sc.nextInt();
+			lim = sc.nextInt();
+			food = new int[N][2];
+			for(int i = 0; i < N; i++) {
+				food[i][0] = sc.nextInt();//점수
+				food[i][1] = sc.nextInt();//칼로리
+			}
+			
+			for(int i = 0; i < N; i++) 
+				dfs(i, food[i][1], food[i][0]);
+			
+			System.out.println("#" + tc + " " + maxScore);
+			maxScore = 0;
+		}
+		
+		sc.close();
+	}
+
 }
