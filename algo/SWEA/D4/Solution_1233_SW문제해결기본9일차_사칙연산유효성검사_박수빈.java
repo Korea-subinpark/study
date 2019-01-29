fabc45a0b991fdab649e106d350716e21bc85685
Korea-subinpark <45931854+Korea-subinpark@users.noreply.github.com>
2019-01-29 15:28:12 +0900

    Add files via upload

---
+++ algo/SWEA/D4/Solution_1233_SW문제해결기본9일차_사칙연산유효성검사_박수빈.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1233_SW문제해결기본9일차_사칙연산유효성검사_박수빈 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int tc = 1; tc <= 10; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int ans = 1;
+			char[] tree = new char[256];
+			for(int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				int num = Integer.parseInt(st.nextToken());
+				if(num == 1) {//루트 검사
+					char c = st.nextToken().charAt(0);
+					if(48 <= c && c >= 57) {//루트가 숫자일 경우 실패
+						ans = 0;
+					}
+				}
+				if(st.countTokens() == 1) {
+					char c = st.nextToken().charAt(0);
+					if(c < 48 || c > 57) {//잎 노드가 연산자일 경우 실패
+						ans = 0;
+					}
+				}
+			}
+			System.out.println("#" + tc + " " + ans);
+		}
+	}
+
+}
