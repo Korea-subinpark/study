192b3d08aeb10f786c77df79b3a8b32d50f8cee0
Korea-subinpark <45931854+Korea-subinpark@users.noreply.github.com>
2019-01-29 15:38:42 +0900

    Update Solution_1233_사칙연산유효성검사.java

--- algo/SWEA/D4/Solution_1233_사칙연산유효성검사.java
+++ algo/SWEA/D4/Solution_1233_사칙연산유효성검사.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 /*
 루트가 숫자일 경우와 잎이 연산자인 경우 실패로 출력
+실패인 경우 flag 값을 true로 하여 더 이상 연산을 수행하지 않고 데이터를 읽기만 
 */
 public class Solution_1233 {
 
@@ -12,20 +13,25 @@ public class Solution_1233 {
 		for(int tc = 1; tc <= 10; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int ans = 1;
+			boolean flag = false;
 			char[] tree = new char[256];
 			for(int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				int num = Integer.parseInt(st.nextToken());
-				if(num == 1) {//루트 검사
-					char c = st.nextToken().charAt(0);
-					if(48 <= c && c >= 57) {//루트가 숫자일 경우 실패
-						ans = 0;
+				if(!flag) {
+					int num = Integer.parseInt(st.nextToken());
+					if(num == 1) {//루트 검사
+						char c = st.nextToken().charAt(0);
+						if(48 <= c && c >= 57) {//루트가 숫자일 경우 실패
+							ans = 0;
+							flag = true;
+						}
 					}
-				}
-				if(st.countTokens() == 1) {
-					char c = st.nextToken().charAt(0);
-					if(c < 48 || c > 57) {//잎 노드가 연산자일 경우 실패
-						ans = 0;
+					if(st.countTokens() == 1) {
+						char c = st.nextToken().charAt(0);
+						if(c < 48 || c > 57) {//잎 노드가 연산자일 경우 실패
+							ans = 0;
+							flag = true;
+						}
 					}
 				}
 			}
