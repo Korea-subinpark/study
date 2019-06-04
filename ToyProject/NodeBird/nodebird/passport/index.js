a49351c83b6612bda1449dfe88f6fb55bc9ebe5f
parksubin <subinpark.re@gmail.com>
2019-06-04 15:01:18 +0900

    multer 이미지 업로드

--- ToyProject/NodeBird/nodebird/passport/index.js
+++ ToyProject/NodeBird/nodebird/passport/index.js
@@ -3,7 +3,7 @@ const kakao = require('./kakaoStrategy');
 const { User } = require('../models');
 
 module.exports = (passport) => {
-    passport.serializerUser((user, done) => { //serializeUser 함수는 req.session 객체에 어떤 데이터를 저장할지 선택한다
+    passport.serializeUser((user, done) => { //serializeUser 함수는 req.session 객체에 어떤 데이터를 저장할지 선택한다
         done(null, user.id); //done 첫 번째 인자는 err 발생 시 사용, 사용자 정보를 모두 세션에 저장하면 용량이 커지고 데이터 일관성에 문제가 생기므로 id만 저장
     });
 
